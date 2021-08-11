b7dc45793ceb2767991202ef690ceb61eae01f65
NeoLiu <theideasaler@gmail.com>
2021-08-11 23:07:57 +1000

    update structure

---
+++ lib/answer.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final Function onPress;
+  final String text;
+
+Answer(this.onPress, this.text);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: onPress,
+        child: Text(text),
+        style: ElevatedButton.styleFrom(
+          primary: Colors.blue,
+          onPrimary: Colors.white,
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './question.dart';
+import 'question.dart';
+import 'answer.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,6 +13,24 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final questions = [
+    {
+      'question': "What's your first pet's name?",
+      'answer': ['Rice', 'Four Try'],
+    },
+    {
+      'question': "Whats's your first pet's birthday?",
+      'answer': ['2021-01-25', '2021-03-26', '2021-01-08'],
+    },
+    {
+      'question': 'How many pets do you have?',
+      'answer': ['1', '2', '3'],
+    },
+    {
+      'question': 'How many times do you feed your pet per day?',
+      'answer': ['2', '3', '4', '5'],
+    },
+  ];
   var _currentQuestionIndex = 0;
 
   _onConfirmCliked() {
@@ -23,12 +42,6 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext ctx) {
-    var questions = [
-      "What's your first pet's name?",
-      "Whats's your first pet's birthday?",
-      "How many pets do you have?",
-      "How many times do you feed your pet per day?",
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -37,23 +50,20 @@ class _MyAppState extends State<MyApp> {
         body: Column(children: [
           Row(children: [
             Expanded(
-              child: Question(questions[_currentQuestionIndex]),
+              child: Question(questions[_currentQuestionIndex]['question']),
             )
           ]),
-          Row(
-            children: [
-              Expanded(
-                child: Center(
-                  child: ElevatedButton(
-                    onPressed: _onConfirmCliked,
-                    child: Text('Confirm'),
-                    style: ElevatedButton.styleFrom(
-                        primary: Colors.blue, onPrimary: Colors.white),
-                  ),
-                ),
-              ),
-            ],
-          )
+          ...(questions[_currentQuestionIndex]['answer'] as List<String>)
+              .map((ans) => Row(
+                    children: [
+                      Expanded(
+                        child: Center(
+                          child: Answer(_onConfirmCliked, ans),
+                        ),
+                      )
+                    ],
+                  ))
+              .toList(),
         ]),
       ),
     );
